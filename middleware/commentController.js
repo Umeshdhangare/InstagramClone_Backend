a9e2b4eb59b981be73e5ba2906884e53e95d2278
Umesh <dhangareumesh@gmail.com>
2023-09-24 01:26:09 +0530

    Update AddComment Handler

--- middleware/commentController.js
+++ middleware/commentController.js
@@ -2,46 +2,46 @@ const Comment = require("../models/comment");
 const Post = require("../models/posts");
 
 const addComment = async (req, res) => {
-    try {
-        const {...comment} = req.body;
-        const postId = req.params.postId;
-        comment.user = req.user._id;
-        const commentosave = new Comment(comment);
-        const savedcomment = await commentosave.save();
+	try {
+		const { ...comment } = req.body;
+		const postId = req.body.postId;
+		comment.user = req.user._id;
+		const commentosave = new Comment(comment);
+		const savedcomment = await commentosave.save();
 
-        await Post.findOneAndUpdate(
-            {_id:postId},
-            {$push: {comments: savedcomment._id}}
-        );
+		await Post.findOneAndUpdate(
+			{ _id: postId },
+			{ $push: { comments: savedcomment._id } }
+		);
 
-        res.status(200).send({
-            status:"success",
-            message: "Comment has been created"
-        });
-    } catch(e) {
-        res.status(500).send({
-            status:"failure",
-            message:e.message
-        })
-    }
-}
+		res.status(200).send({
+			status: "success",
+			message: "Comment has been created",
+		});
+	} catch (e) {
+		res.status(500).send({
+			status: "failure",
+			message: e.message,
+		});
+	}
+};
 
-const getbyPostId = async (req,res) => {
-    const postId = req.params.postId;
-    try {
-        const post = await Post.findOne({
-            _id: postId
-        }).populate("comments");
-        res.status(200).send({
-            status:"success",
-            comments:post.comments
-        });
-    } catch(e) {
-        res.status(500).send({
-            status:"failure",
-            message:e.message
-        })
-    }
-}
+const getbyPostId = async (req, res) => {
+	const postId = req.params.postId;
+	try {
+		const post = await Post.findOne({
+			_id: postId,
+		}).populate("comments");
+		res.status(200).send({
+			status: "success",
+			comments: post.comments,
+		});
+	} catch (e) {
+		res.status(500).send({
+			status: "failure",
+			message: e.message,
+		});
+	}
+};
 
-module.exports = {addComment, getbyPostId};
+module.exports = { addComment, getbyPostId };
